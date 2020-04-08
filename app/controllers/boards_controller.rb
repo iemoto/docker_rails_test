9ac3f1011bbcab1a0965af288f6ed5974ef563ef
iemoto <57856680+iemoto@users.noreply.github.com>
2020-04-08 15:38:17 +0900

    before_action add

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,6 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: [:show, :edit, :update, :destroy]
+
   def index
      @boards = Board.all
   end
@@ -8,8 +10,8 @@ class BoardsController < ApplicationController
   end
   
   def create
-    Board.create(board_params)
-    redirect_to boards_path
+    board = Board.create(board_params)
+    redirect_to board
   end
 
   def show
@@ -21,13 +23,22 @@ class BoardsController < ApplicationController
   end
 
   def update
-    board = Board.find(params[:id])
-    board.update(board_params)
+    @board.update(board_params)
 
-    redirect_to board
+    redirect_to @board
+  end
+
+  def destroy
+    @board.delete
+
+    redirect_to boards_path
   end
 
   private
+
+  def set_target_board
+    @board = Board.find(params[:id])
+  end
 
   def board_params
     params.require(:board).permit(:name, :title, :body)
