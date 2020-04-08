fd3d3981dea4a605295276916b5ed9971a368924
iemoto <57856680+iemoto@users.noreply.github.com>
2020-04-08 18:17:51 +0900

    comments mode and controller add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
+# == Route Map
+#
+#     Prefix Verb   URI Pattern                Controller#Action
+#     boards GET    /boards(.:format)          boards#index
+#            POST   /boards(.:format)          boards#create
+#  new_board GET    /boards/new(.:format)      boards#new
+# edit_board GET    /boards/:id/edit(.:format) boards#edit
+#      board GET    /boards/:id(.:format)      boards#show
+#            PATCH  /boards/:id(.:format)      boards#update
+#            PUT    /boards/:id(.:format)      boards#update
+#            DELETE /boards/:id(.:format)      boards#destroy
+
 Rails.application.routes.draw do
+  get 'comments/create'
+
+  get 'comments/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
   resources :boards
 end
